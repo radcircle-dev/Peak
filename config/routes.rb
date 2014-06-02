f66dc0bad486d3b11f454b19af0e37690d9fdce4
austinRadcircle <austin@radcircle.com>
2014-06-02 09:22:17 -0500

    Testing New repo

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :welcome
   resources :posts
   root "posts#index"
+  #this is a change
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
