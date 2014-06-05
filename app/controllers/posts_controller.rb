224b44b6afc33a83bfcaa1ab191cde6b85da59d3
austinRadcircle <austin@radcircle.com>
2014-06-05 09:20:47 -0500

    add destroy function on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,13 +17,19 @@ class PostsController < ApplicationController
   def create
   	@post = Post.new(post_params)
   	if @post.save
+     
   		redirect_to @post
   	else
+
   		render 'new'
   	end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   def edit
