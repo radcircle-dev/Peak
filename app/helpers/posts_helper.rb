8cea77d8c59307f65d8f4759584c8b7e78077726
austinRadcircle <austin@radcircle.com>
2014-06-11 13:50:34 -0500

    comments are working now

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
 	def author_(post)
-		raw("<div class=""post_details"">Posted By: #{post.user} | </div>")
+		raw("<div class=""post_details"">Posted By: #{post.user} | #{link_to(pluralize(post.comments.size, 'Comment'), post_path(post, anchor: 'commentlabel'))}</div>")
 	end
 end
