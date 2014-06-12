0d1003972b08875e0c0c6a4c736bd4fa9bc320bd
mazib <m_azib@hotmail.com>
2014-06-12 12:07:18 +0200

    login + signup prealpha + homepage 0.25
    login = 0.5 prealpha
    signup = 0.5 prealpha
    homepage work begun

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,17 @@
 module ApplicationHelper
 	def user_logged_in_msg
 		str = ''
-		str += '<div class = "userloggedin">'
+		str += '<div class="row">
+		<div class="col-xs-12 col-md-2 hidden-xs"></div> <!-- left side -->
+			<div class="col-xs-12 col-md-8"><div class = "userloggedin">'
 		if user_signed_in?
 			str += "<p id = ""user_info"">#{current_user}<br>#{link_to('Logout', destroy_user_session_path, :method => :delete)}</p>"
 		else
 			str += "<p id = ""user_info"">#{link_to('Login', new_user_session_path)} | #{link_to('Register', new_user_registration_path)}</p>"
 		end
-		str += '</div>'
+		str += '			</div>
+		<div class="col-xs-12 col-md-2 hidden-xs"></div> <!-- right side -->
+	</div></div>'
 		raw(str)
 	end
 
