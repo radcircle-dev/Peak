ac1bdaf16c53f75fc00e7670f0152c817b18ea1b
austinRadcircle <austin@radcircle.com>
2014-06-16 11:16:56 -0500

    added categories, paperclip gem that is not set up

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
 	def author_(post)
-		raw("<div class=""post_details"">Posted By: #{post.user} | #{link_to(pluralize(post.comments.size, 'Comment'), post_path(post, anchor: 'commentlabel'))}</div>")
+		raw("<div class=""post_details""> Categories: #{post.categories.map { |c| link_to(c.title, c) }.join(', ')} <br> Posted By: #{post.user} | #{link_to(pluralize(post.comments.size, 'Comment'), post_path(post, anchor: 'commentlabel'))}</div>")
 	end
 end
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,4 @@
+require "paperclip"
+
+Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.8.9-Q16'
+Paperclip.options[:swallow_stderr] = false
