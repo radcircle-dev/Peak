f92055281d01491cedf57997c45bee862a5032ff
austinRadcircle <austin@radcircle.com>
2014-06-18 14:11:26 -0500

    sign in/out as admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,17 @@ module ApplicationHelper
 	end
 
 	def admin_logged_in_msg
+		str = ''
+		str += '<div class="row">
+		<div class="col-xs-12 col-md-2 hidden-xs"></div> <!-- left side -->
+			<div class="col-xs-12 col-md-8"><div class = "adminloggedin">'
+		if admin_signed_in?
+			str += "<p id = ""user_info"">#{current_admin}<br>#{link_to('Logout', destroy_admin_session_path, :method => :delete)}</p>"
 		
+		end
+		str += '			</div>
+		<div class="col-xs-12 col-md-2 hidden-xs"></div> <!-- right side -->
+	</div></div>'
+		raw(str)
 	end
 end
