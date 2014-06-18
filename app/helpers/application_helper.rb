266001a03561908c2ab1225e53bf8aa7d53c2b3c
mazib <m_azib@hotmail.com>
2014-06-18 22:01:33 +0200

    general styling
    a big update for general styling and improving the UI of the site.
    affected: login/register  + alignment
    fixing some grid bugs and layout inconsistencies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,15 @@ module ApplicationHelper
 	def user_logged_in_msg
 		str = ''
 		str += '<div class="row">
-		<div class="col-xs-12 col-md-2 hidden-xs"></div> <!-- left side -->
-			<div class="col-xs-12 col-md-8"><div class = "userloggedin">'
+		<div class="col-xs-12 col-md-3 hidden-xs"></div> <!-- left side -->
+			<div class="col-xs-12 col-md-6"><div class = "userloggedin">'
 		if user_signed_in?
 			str += "<p id = ""user_info"">#{current_user}<br>#{link_to('Logout', destroy_user_session_path, :method => :delete)}</p>"
 		else
 			str += "<p id = ""user_info"">#{link_to('Login', new_user_session_path)} | #{link_to('Register', new_user_registration_path)}</p>"
 		end
 		str += '			</div>
-		<div class="col-xs-12 col-md-2 hidden-xs"></div> <!-- right side -->
+		<div class="col-xs-12 col-md-3 hidden-xs"></div> <!-- right side -->
 	</div></div>'
 		raw(str)
 	end
