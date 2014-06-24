1bb5137f8b2514abeba305f8160572904b16c2cf
austinRadcircle <austin@radcircle.com>
2014-06-23 20:40:09 -0500

    some stuff here

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
 	def user_logged_in_msg
 		str = ''
-		str += '<div class="row">
-		<div class="col-xs-12 col-md-3 hidden-xs"></div> <!-- left side -->
-			<div class="col-xs-12 col-md-6"><div class = "userloggedin">'
+		str += '<div class="row">'
+		str += '<div class="col-xs-12 col-md-3 hidden-xs"></div>'
+		str +=  '<!-- left side -->  <div class="col-xs-12 col-md-6"><div class = "userloggedin">'
+		
 		if user_signed_in?
 			str += "<p id = ""user_info"">Logged in as #{current_user}<br>#{link_to('Logout', destroy_user_session_path, :method => :delete)}</p>"
 		else
 			str += "<p id = ""user_info"">#{link_to('Login', new_user_session_path)} or #{link_to('Sign Up', new_user_registration_path)}</p>"
 		end
-		str += '			</div>
-		<div class="col-xs-12 col-md-3 hidden-xs"></div> <!-- right side -->
-	</div></div>'
+		str += '</div>'
+		str += '<div class="col-xs-12 col-md-3 hidden-xs"></div> <!-- right side --></div></div>'
 		raw(str)
 	end
 
@@ -24,9 +24,7 @@ module ApplicationHelper
 			str += "<p id = ""user_info"">#{current_admin}<br>#{link_to('Logout', destroy_admin_session_path, :method => :delete)}</p>"
 		
 		end
-		str += '			</div>
-		<div class="col-xs-12 col-md-2 hidden-xs"></div> <!-- right side -->
-	</div></div>'
+		str += '</div><div class="col-xs-12 col-md-2 hidden-xs"></div> <!-- right side --></div></div>'
 		raw(str)
 	end
 end
