a96dbfecd59620ecfd4a96e47ff652721d77b813
austinRadcircle <austin@radcircle.com>
2014-06-27 00:16:31 -0500

    Feedback, Search, Vanity urls

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,16 @@ class Post < ActiveRecord::Base
 		"#{title}"
 	end
 
+	def self.search(query)
+		where("title like ?", "%#{query}%")
+	end
+
+	def to_param
+		"#{id}-#{slug}"
+	end
+
+	def slug
+		title.downcase.gsub(" ", "-")
+	end
+
 end
